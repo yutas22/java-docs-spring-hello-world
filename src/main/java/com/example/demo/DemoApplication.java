f99225abffa57ad5ed6ea0cb263dd4ef965020f0
yutas22 <113437620+yutas22@users.noreply.github.com>
2022-09-14 17:31:39 +0900

    Add HTTP Request

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,12 +5,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// add 2022/09/14
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.nio.charset.StandardCharsets;
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
+	//public static void main(String[] args) {
+	//	SpringApplication.run(DemoApplication.class, args);
+	//}
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		try {
+			HttpRequest request = HttpRequest
+					.newBuilder(URI.create("https://datadoghq.com"))
+					.build();
+ 
+			BodyHandler<String> bodyHandler = HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8);
+			HttpResponse<String> response = HttpClient.newBuilder().build().send(request, bodyHandler);
+			System.out.println(response.body());
+			SpringApplication.run(DemoApplication.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@RequestMapping("/")
